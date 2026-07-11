2adbde0f17dc295c25d9a64bf37e13c7a9b7f512
Abdelrahman B. <99421749+AbdelrahmanBerchan@users.noreply.github.com>
2026-07-11 14:12:17 +0300

    Add profile and address management functions

--- src/webview-preload-settings.js
+++ src/webview-preload-settings.js
@@ -8,9 +8,19 @@ contextBridge.exposeInMainWorld('electronAPI', {
 
   getSettings: () => ipcRenderer.invoke('get-settings'),
   getSettingsWindowBootstrap: () => ipcRenderer.sendSync('axis-settings-window-bootstrap'),
+  getSettingsProfileBootstrap: () => ipcRenderer.sendSync('axis-settings-profile-bootstrap'),
+  getSystemUiTheme: () => ipcRenderer.invoke('get-system-ui-theme'),
+  onSystemUiThemeChanged: (callback) => {
+    const handler = (_event, theme) => callback(theme);
+    ipcRenderer.on('system-ui-theme-changed', handler);
+    return () => ipcRenderer.removeListener('system-ui-theme-changed', handler);
+  },
   setSetting: (key, value) => ipcRenderer.invoke('set-setting', key, value),
   getSitePermissionOverrides: () => ipcRenderer.invoke('get-site-permission-overrides'),
   setSitePermissionOverrides: (obj) => ipcRenderer.invoke('set-site-permission-overrides', obj),
+  getSettingsEditingContext: () => ipcRenderer.invoke('get-settings-editing-context'),
+  setSettingsEditingProfile: (profileId) =>
+    ipcRenderer.invoke('set-settings-editing-profile', profileId),
   sendSettingsUpdated: () => ipcRenderer.send('settings-updated'),
   openSettingsWindow: (tab) => ipcRenderer.invoke('open-settings-window', tab),
   openUrlInBrowser: (url) => ipcRenderer.invoke('open-url-in-browser', url),
@@ -41,6 +51,10 @@ contextBridge.exposeInMainWorld('electronAPI', {
   vaultGetCard: (id) => ipcRenderer.invoke('axis-vault-get-card', id),
   vaultSaveCard: (entry) => ipcRenderer.invoke('axis-vault-save-card', entry),
   vaultDeleteCard: (id) => ipcRenderer.invoke('axis-vault-delete-card', id),
+  vaultListAddresses: () => ipcRenderer.invoke('axis-vault-list-addresses'),
+  vaultGetAddress: (id) => ipcRenderer.invoke('axis-vault-get-address', id),
+  vaultSaveAddress: (entry) => ipcRenderer.invoke('axis-vault-save-address', entry),
+  vaultDeleteAddress: (id) => ipcRenderer.invoke('axis-vault-delete-address', id),
 
   getShortcuts: () => ipcRenderer.invoke('get-shortcuts'),
   getShortcutOverrides: () => ipcRenderer.invoke('get-shortcut-overrides'),
@@ -52,6 +66,43 @@ contextBridge.exposeInMainWorld('electronAPI', {
 
   onSettingsUpdated: (callback) =>
     ipcRenderer.on('settings-updated', (_event, data) => callback(data)),
+  onSettingsStoreUpdated: (callback) =>
+    ipcRenderer.on('axis-settings-store-updated', () => callback()),
   onSwitchSettingsTab: (callback) =>
-    ipcRenderer.on('switch-settings-tab', (_event, tab) => callback(tab))
+    ipcRenderer.on('switch-settings-tab', (_event, tab) => callback(tab)),
+  onSettingsEditingProfileChanged: (callback) => {
+    const handler = (_event, data) => callback(data);
+    ipcRenderer.on('settings-editing-profile-changed', handler);
+    return () => ipcRenderer.removeListener('settings-editing-profile-changed', handler);
+  },
+
+  getProfiles: () => ipcRenderer.invoke('get-profiles'),
+  getCurrentProfileId: () => ipcRenderer.invoke('get-current-profile-id'),
+  createProfile: (payload) => ipcRenderer.invoke('create-profile', payload),
+  updateProfile: (payload) => ipcRenderer.invoke('update-profile', payload),
+  deleteProfile: (profileId) =>
+    ipcRenderer.invoke(
+      'delete-profile',
+      typeof profileId === 'object' ? profileId : { id: profileId }
+    ),
+  reorderProfiles: (orderedIds) => ipcRenderer.invoke('reorder-profiles', orderedIds),
+  switchProfileInWindow: (profileId) => ipcRenderer.invoke('switch-profile-in-window', profileId),
+  openOrFocusProfileWindow: (profileId) => ipcRenderer.invoke('open-or-focus-profile-window', profileId),
+  onProfilesUpdated: (callback) => {
+    const handler = () => callback();
+    ipcRenderer.on('profiles-updated', handler);
+    return () => ipcRenderer.removeListener('profiles-updated', handler);
+  },
+  getProfileGlobalSettings: () => ipcRenderer.invoke('get-profile-global-settings'),
+  setProfileGlobalSetting: (key, value) =>
+    ipcRenderer.invoke('set-profile-global-setting', key, value),
+  getProfilesOverviewForWindow: () => ipcRenderer.invoke('get-profiles-overview-for-window'),
+  exportAxisProfile: (profileId) => ipcRenderer.invoke('export-axis-profile', profileId),
+  importAxisProfileBackup: () => ipcRenderer.invoke('import-axis-profile-backup'),
+  listImportableBrowsers: () => ipcRenderer.invoke('list-importable-browsers'),
+  listBrowserImportProfiles: (browserId) => ipcRenderer.invoke('list-browser-import-profiles', browserId),
+  pickBrowserProfileFolder: () => ipcRenderer.invoke('pick-browser-profile-folder'),
+  inspectImportProfileFolder: (folderPath) => ipcRenderer.invoke('inspect-import-profile-folder', folderPath),
+  importBrowserProfile: (payload) => ipcRenderer.invoke('import-browser-profile', payload),
+  previewBrowserImport: (payload) => ipcRenderer.invoke('preview-browser-import', payload)
 });
